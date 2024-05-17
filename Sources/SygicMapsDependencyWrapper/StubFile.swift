2a7a94502a1f45e42afa94889ae7a621dd26fb34
Oleksandr Lavrushko <olavrushko@sygic.com>
2024-05-17 11:47:37 +0200

    Fix issue with ignoring SygicAuth dependency when only SygicMaps is added to project

---
+++ Sources/SygicMapsDependencyWrapper/StubFile.swift
@@ -0,0 +1,5 @@
+// This stub source file helps to provide dependencies for binary target.
+// Binary target cannot have dependencies itself, but target with source files can.
+// That's why we add a stub target with a stub source file and declare dependencies for it.
+
+import Foundation
